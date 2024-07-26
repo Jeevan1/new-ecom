33cee5935cc145bc8c63f4233482af4b78b8394b
Jeevan Stha <shresthaj1986@gmail.com>
2024-07-26 22:33:12 +0545

    updated product card css

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -32,19 +32,34 @@ const Page = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 mx-auto w-80 mt-5">
+    <div className="grid grid-cols-3 mx-auto w-80 my-5 gap-4 ">
       {products.map((item: any) => (
         <div
           key={item.id}
-          className="border flex flex-col justify-center items-center"
+          className="border flex flex-col justify-center items-center p-3 rounded-md shadow-md cursor-pointer"
         >
-          <p>{item.title}</p>
+          <p className="font-bold text-xl">{item.title}</p>
           <Image
             src={item.thumbnail}
             alt={item.title}
             width={200}
             height={200}
           />
+
+          <p className="text-sm text-center line-clamp-2">{item.description}</p>
+          <div className="flex items-center gap-4">
+            <p>${item.price}</p>
+            <p className="text-sm line-through text-green-500">
+              ${item.discountPercentage}
+            </p>
+          </div>
+          <div className="flex gap-3 justify-center items-center">
+            <p>{item.category}</p>
+            <p>stock: {item.stock}</p>
+            <p>rating: {item.rating}</p>
+          </div>
+
+          <p>brand: {item.brand}</p>
         </div>
       ))}
     </div>
